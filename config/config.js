31a1b2f3f0b00cb1b79d8c9926bc61892558f667
abdultalif <abdultalif85@gmail.com>
2023-10-30 00:31:44 +0700

    setup ORM sequelize

---
+++ config/config.js
@@ -0,0 +1,35 @@
+require(dotenv).config();
+
+
+const {
+  DB_HOSTNAME,
+  DB_USERNAME,
+  DB_PASSWORD,
+  DB_DATABASE,
+  DB_DIALECT
+} = process.env;
+
+module.exports =
+{
+  "development": {
+    "username": DB_USERNAME,
+    "password": DB_PASSWORD,
+    "database": DB_DATABASE,
+    "host": DB_HOSTNAME,
+    "dialect": DB_DIALECT
+  },
+  "test": {
+    "username": DB_USERNAME,
+    "password": DB_PASSWORD,
+    "database": DB_DATABASE,
+    "host": DB_HOSTNAME,
+    "dialect": DB_DIALECT
+  },
+  "production": {
+    "username": DB_USERNAME,
+    "password": DB_PASSWORD,
+    "database": DB_DATABASE,
+    "host": DB_HOSTNAME,
+    "dialect": DB_DIALECT
+  }
+}

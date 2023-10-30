f82d9a2be6a605d6bcd8adf8291461e4505ef6a0
abdultalif <abdultalif85@gmail.com>
2023-10-30 09:33:31 +0700

    update data

--- routes/products.js
+++ routes/products.js
@@ -19,4 +19,25 @@ router.post('/', async (req, res) => {
     res.json(product);
 });
 
+
+router.put('/:id', async (req, res) => {
+    const id = req.params.id;
+    let product = await Product.findByPk(id);
+    if (!product) {
+        return res.json({ message: 'Product Not Found' });
+    }
+    const schema = {
+        name: "string|optional",
+        brand: "string|optional",
+        description: "string|optional"
+    }
+    const validate = v.validate(req.body, schema);
+    if (validate.length) {
+        return res.status(400).json(validate);
+    }
+
+    product = await product.update(req.body);
+    res.json(product);
+})
+
 module.exports = router;
